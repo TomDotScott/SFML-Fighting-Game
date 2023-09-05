551551471a20a68930f7d69a8d2331bfb2df5c12
Tom Scott <tom.scott45456@gmail.com>
2023-09-05 21:40:37 +0100

    Clock Class

---
+++ FightingGame/Clock.cpp
@@ -0,0 +1,36 @@
+﻿#include "Clock.h"
+
+#include <iostream>
+
+Clock& Clock::Get()
+{
+	static Clock* instance = nullptr;
+
+	if (instance == nullptr)
+	{
+		instance = new Clock();
+	}
+
+	return *instance;
+}
+
+float Clock::DeltaTime() const
+{
+	return m_delta.count() / powf(10.f, 9.f);
+}
+
+Clock::Clock() :
+	m_timeLastUpdate(std::chrono::high_resolution_clock::now()),
+	m_delta()
+{
+}
+
+void Clock::Update()
+{
+	const std::chrono::high_resolution_clock::time_point currentTime = std::chrono::high_resolution_clock::now();
+	m_delta = currentTime - m_timeLastUpdate;
+
+	// std::cout << m_delta.count() << std::endl;
+
+	m_timeLastUpdate = currentTime;
+}
---
+++ FightingGame/Clock.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <chrono>
+
+class Clock
+{
+	friend class Game;
+
+public:
+	static Clock& Get();
+
+	float DeltaTime() const;
+
+private:
+	Clock();
+	void Update();
+
+	std::chrono::high_resolution_clock::time_point m_timeLastUpdate;
+	std::chrono::high_resolution_clock::duration m_delta;
+};
